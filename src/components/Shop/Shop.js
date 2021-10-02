30866ef326774ac60e1adb575633b23f1d75b4e8
Springhawk27 <sajjad0mahmud@gmail.com>
2021-10-02 10:18:50 +0600

    product quantity adding method solved

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -69,7 +69,18 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // console.log(product);
-        const newCart = [...cart, product];
+        const exists = cart.find(pd => pd.key === product.key);
+        let newCart = [];
+        if (exists) {
+            const rest = cart.filter(pd => pd.key !== product.key);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, product];
+        }
+        else {
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        // const newCart = [...cart, product];
         setCart(newCart);
         //save to local storage for now
         addToDb(product.key);
